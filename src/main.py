eb50bee89991de2ba40b4bf6d652f8a300c2dc38
KevinChiu <fsd83811@gmail.com>
2020-11-28 20:09:06 +0800

    cv2_draw

--- src/main.py
+++ src/main.py
@@ -10,14 +10,24 @@ import cv2
 cap = cv2.VideoCapture(2)
 while(True):
     ret, frame = cap.read()
+    img = frame
 
     # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    qrDecode = decode(frame)
-    for i in qrDecode:
-        print(i.rect)
+    qrDecode = decode(img)
+    for qrLoop in qrDecode:
+        print(qrLoop.rect)
 
-    cv2.imshow('frame', frame)
+        qrRectX, qrRectY, qrRectW, qrRectH = qrLoop.rect.left, qrLoop.rect.top, qrLoop.rect.width, qrLoop.rect.height
+        cv2.circle(img, (int(qrRectX + qrRectW / 2),
+                         int(qrRectY + qrRectH / 2)), 5, (0, 255, 0), -1)
+        cv2.rectangle(img, (qrRectX, qrRectY),
+                      (qrRectX+qrRectW, qrRectY+qrRectH), (255, 0, 0), 8)
+
+        for qrPolygonLoop in qrLoop.polygon:
+            cv2.circle(img, (qrPolygonLoop.x, qrPolygonLoop.y), 5, (0, 0, 255), -1)
+
+    cv2.imshow('img', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- src/pyzbar_ex.py
+++ src/pyzbar_ex.py
@@ -7,7 +7,9 @@ print(decode(Image.open('img/c0FVK.jpg')))
 from pyzbar.pyzbar import decode
 import cv2
 qq = decode(cv2.imread('img/34455166.png'))
+print(qq[0])
 print(qq[0].rect)
+print("polygon",qq[0].polygon[0].x)
 
 frame =  cv2.imread('img/34455166.png')
 qrDecode = decode(frame)
